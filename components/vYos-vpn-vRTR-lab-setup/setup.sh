2b4d8fc57c135ab8044121067fcf6934dc7d6a95
arikyakir <44773905+arikyakir@users.noreply.github.com>
2019-03-04 12:10:16 +0200

    changed to the new vyos-vpn service
    2 service instances of hub & spoke and 3rd config service towards both

--- components/vYos-vpn-vRTR-lab-setup/setup.sh
+++ components/vYos-vpn-vRTR-lab-setup/setup.sh
@@ -2,8 +2,9 @@ cfy profiles use localhost -u admin -p admin -t default_tenant
 
 # Upload blueprints
 ###
-cfy blueprints upload -n vyos-vnf-branch.yaml    -b "vYos-vnf-branch-VPN-blueprint"                    https://github.com/Cloudify-PS/vyos-sdwan-vnf-blueprint/archive/master.zip
+cfy blueprints upload -n vyos-vnf-hub-baseline.yaml    -b "vYos-hub-bp"                    https://github.com/cloudify-examples/vyos-vnf-vpn-blueprint/archive/v1.zip
 
-cfy blueprints upload -n vyos-vnf-hq-baseline.yaml    -b "vYos-vnf-hub-VPN-blueprint"                    https://github.com/Cloudify-PS/vyos-sdwan-vnf-blueprint/archive/master.zip
+cfy blueprints upload -n vyos-vnf-branch-baseline.yaml    -b "vYos-branch-bp"                    https://github.com/cloudify-examples/vyos-vnf-vpn-blueprint/archive/v1.zip
 
+cfy blueprints upload -n vyos-vpn-config-service.yaml    -b "vYos-vpn-config-bp"                    https://github.com/cloudify-examples/vyos-vnf-vpn-blueprint/archive/v1.zip
 
