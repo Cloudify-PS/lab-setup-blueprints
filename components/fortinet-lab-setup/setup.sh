0bf8dc5151cdf27e75b91980166932c58cb7feba
Arik Yakir <44773905+arikyakir@users.noreply.github.com>
2019-03-20 15:45:06 +0200

    Update setup.sh

--- components/fortinet-lab-setup/setup.sh
+++ components/fortinet-lab-setup/setup.sh
@@ -6,6 +6,12 @@ cfy profiles use localhost -u admin -p admin -t default_tenant
 #cfy blueprints upload -n openstack-vm-blueprint-ws.yaml -b "private-webserver-bp" https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/Blueprints/NFVLAb/private-webserver-blueprint-master.zip
 cfy blueprints upload https://github.com/cloudify-examples/nodecellar-auto-scale-auto-heal-blueprint/archive/master.zip -n openstack.yaml -b "private-webserver-bp" --validate 
 cfy deployments create -b "private-webserver-bp" private-webserver
+
+#### upload the vFW BPs prior to isntall of HTTPD
+cfy blueprints upload https://github.com/Cloudify-PS/fortigate-pf-vnf-blueprint/archive/master.zip -n fortigate-vnf-baseline-bp.yaml -b  fortigate-vnf-baseline-bp --validate
+cfy blueprints upload https://github.com/Cloudify-PS/fortigate-pf-vnf-blueprint/archive/master.zip -n fortigate-vnf-portforward-bp.yaml -b  fortigate-portforward-vnf-config --validate
+
+#### install HTTPD
 cfy executions start install -d "private-webserver"
 
 # Upload blueprints
@@ -14,8 +20,6 @@ cfy executions start install -d "private-webserver"
 #cfy blueprints upload -n openstack-vm-lan.yaml  -b "openstack-vnf-infra"                 https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/Blueprints/NFVLAb/nfv-scenario-blueprint-master.zip
 #cfy blueprints upload -n fortigate-vnf-portforward-bp.yaml -b "fortigate-portforward-bp" https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/Blueprints/NFVLAb/nfv-scenario-blueprint-master.zip
 
-cfy blueprints upload https://github.com/Cloudify-PS/fortigate-pf-vnf-blueprint/archive/master.zip -n fortigate-vnf-baseline-bp.yaml -b  fortigate-vnf-baseline-bp --validate
-cfy blueprints upload https://github.com/Cloudify-PS/fortigate-pf-vnf-blueprint/archive/master.zip -n fortigate-vnf-portforward-bp.yaml -b  fortigate-portforward-vnf-config --validate
 
 
 # Create Deployments for Stage1
