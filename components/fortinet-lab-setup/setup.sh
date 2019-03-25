7c69c3f316fb4141ee979999cdbb97910363f90a
Arik Yakir <44773905+arikyakir@users.noreply.github.com>
2019-03-25 22:35:22 +0200

    added upload plugin 2.14.8 for old openstack
    changed also BP upload to version using that plugin

--- components/fortinet-lab-setup/setup.sh
+++ components/fortinet-lab-setup/setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 ### This line is required to set the prifile
 cfy profiles use localhost -u admin -p admin -t default_tenant
+
+# upload openstack v2.14.7 for old openstack version and plugin
+cfy plugins upload http://repository.cloudifysource.org/cloudify/wagons/cloudify-openstack-plugin/2.14.8/cloudify_openstack_plugin-2.14.8-py27-none-linux_x86_64-centos-Core.wgn -y https://github.com/cloudify-cosmo/cloudify-openstack-plugin/releases/download/2.14.8/plugin.yaml
+
 # Install the webserver
 ####
 #cfy blueprints upload -n openstack-vm-blueprint-ws.yaml -b "private-webserver-bp" https://storage.reading-a.openstack.memset.com:8080/swift/v1/ca0c4540c8f84ad3917c40b432a49df8/Blueprints/NFVLAb/private-webserver-blueprint-master.zip
-cfy blueprints upload https://github.com/cloudify-examples/nodecellar-auto-scale-auto-heal-blueprint/archive/master.zip -n openstack.yaml -b "private-webserver-bp" --validate 
+#cfy blueprints upload https://github.com/cloudify-examples/nodecellar-auto-scale-auto-heal-blueprint/archive/master.zip -n openstack.yaml -b "private-webserver-bp" --validate 
+
+#specific version for support openstack kilo and plugin
+cfy blueprints upload https://github.com/cloudify-examples/nodecellar-auto-scale-auto-heal-blueprint/archive/4.3.zip -n openstack.yaml -b "private-webserver-bp" --validate 
+
 
 #### upload the vFW BPs prior to isntall of HTTPD
 cfy blueprints upload https://github.com/Cloudify-PS/fortigate-pf-vnf-blueprint/archive/master.zip -n fortigate-vnf-baseline-bp.yaml -b  fortigate-vnf-baseline-bp --validate
