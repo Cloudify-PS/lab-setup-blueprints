718e815e7c07e3817e3f40f8b67155b44f07b960
Arik Yakir <44773905+arikyakir@users.noreply.github.com>
2019-06-03 15:10:35 +0300

    added ctx logger

--- components/fortinet-lab-setup/setup.sh
+++ components/fortinet-lab-setup/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #### this is modified for lab version based on CFY 4.6
 
+ctx logger info "clear logs - check log at /tmp/lab_status.txt"
 sudo rm -f /tmp/lab_status.txt
 sudo yum install -y  python-netaddr >> /tmp/lab_status.txt 2>&1
 sudo sh -c "  ./opt/mgmtworker/env/bin/activate ; pip install netaddr" >> /tmp/lab_status.txt 2>&1
@@ -9,16 +10,20 @@ sudo sh -c "source /opt/mgmtworker/env/bin/activate ; pip install netaddr" >> /t
 ### This line is required to set the profile
 cfy profiles use localhost -u admin -p admin -t default_tenant >> /tmp/lab_status.txt 2>&1
 
+ctx logger info "Uploading plugins"
 # upload openstack v2.14.7 for old openstack version and plugin
 cfy plugins upload http://repository.cloudifysource.org/cloudify/wagons/cloudify-openstack-plugin/2.14.7/cloudify_openstack_plugin-2.14.7-py27-none-linux_x86_64-centos-Core.wgn -y https://github.com/cloudify-cosmo/cloudify-openstack-plugin/releases/download/2.14.7/plugin.yaml >> /tmp/lab_status.txt 2>&1
 cfy plugins upload http://repository.cloudifysource.org/cloudify/wagons/cloudify-diamond-plugin/1.3.17/cloudify_diamond_plugin-1.3.17-py27-none-linux_x86_64-centos-Core.wgn -y http://www.getcloudify.org/spec/diamond-plugin/1.3.17/plugin.yaml >> /tmp/lab_status.txt 2>&1
 
-
+ctx logger info "Clear old openstaclk-example deployment"
 cfy executions start uninstall -d "openstack-example-network" -p ignore_failure=true >> /tmp/lab_status.txt 2>&1
 cfy deployments delete "openstack-example-network"  >> /tmp/lab_status.txt 2>&1
 cfy blueprints delete "openstack-example-network"  >> /tmp/lab_status.txt 2>&1 &
 
+ctx logger info "Uploading openstack nwtork blueprint"
 cfy blueprints upload https://github.com/cloudify-examples/openstack-example-network/archive/4.5.0.1.zip -n simple-blueprint.yaml -b "openstack-example-network"  >> /tmp/lab_status.txt 2>&1
+
+ctx logger info "Create openstack nwtork deployment"
 cfy deployments create -b "openstack-example-network"  openstack-example-network -i external_network_name=external_network >> /tmp/lab_status.txt 2>&1
 cfy executions start install -d "openstack-example-network" >> /tmp/lab_status.txt 2>&1
 
@@ -28,15 +33,20 @@ cfy executions start install -d "openstack-example-network" >> /tmp/lab_status.t
 #cfy blueprints upload https://github.com/cloudify-examples/nodecellar-auto-scale-auto-heal-blueprint/archive/master.zip -n openstack.yaml -b "private-webserver-bp" --validate
 
 ## upload prive webserver BP
+ctx logger info "Upload webserver blueprint"
 cfy blueprints upload https://github.com/cloudify-examples/nodecellar-auto-scale-auto-heal-blueprint/archive/4.3.zip -n openstack.yaml -b "private-webserver-bp" --validate >> /tmp/lab_status.txt 2>&1
 
 
 #### upload the vFW BPs prior to isntall of HTTPD
+ctx logger info "Upload Fortigate blueprints"
 cfy blueprints upload https://github.com/arikyakir/fortigate-pf-vnf-blueprint/archive/master.zip -n fortigate-vnf-baseline-bp.yaml -b  fortigate-vnf-baseline-bp --validate >> /tmp/lab_status.txt 2>&1
 cfy blueprints upload https://github.com/arikyakir/fortigate-pf-vnf-blueprint/archive/master.zip -n fortigate-vnf-portforward-bp.yaml -b  fortigate-portforward-vnf-config --validate >> /tmp/lab_status.txt 2>&1
 
 #### install HTTPD
+
+ctx logger info "Create private webserver deployment"
 cfy deployments create -b "private-webserver-bp" private-webserver >> /tmp/lab_status.txt 2>&1
+ctx logger info "Install private webserver deployment"
 cfy executions start install -d "private-webserver" >> /tmp/lab_status.txt 2>&1
 
 # Upload blueprints
